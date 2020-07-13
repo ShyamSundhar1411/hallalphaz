ebc36420f5fc857e664969cd9fc9b8cbb9cf643e
Shyam Sundhar <clashwithchiefrpjyt@gmail.com>
2020-07-13 18:24:25 +0530

    Deploy

--- halls/models.py
+++ halls/models.py
@@ -5,8 +5,12 @@ from django.contrib.auth.models import User
 class Hall(models.Model):
     title = models.CharField(max_length = 100)
     user  = models.ForeignKey(User, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.title
 class Video(models.Model):
     title = models.CharField(max_length = 100)
     url = models.URLField()
     youtube_id = models.CharField(max_length = 500)
     hall = models.ForeignKey(Hall,on_delete = models.CASCADE)
+    def __str__(self):
+        return self.title
